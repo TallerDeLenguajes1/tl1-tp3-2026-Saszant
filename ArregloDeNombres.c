1968303620c470d1cf1d039b129e1765cddebef6
Santiago Luciano Medina <medsant0112@gmail.com>
2026-05-05 01:52:16 -0300

    Agrego BuscarNombre por ID

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -4,12 +4,14 @@
 #include <string.h>
 
 void mostrarPersonas(char *vector[], int cantidad);
+void BuscarNombre(char *vector[], int id, int cantidad);
 
 int main(){
 
     char *buff;
     char *nombre;
     char *vector[5];
+    int id;
 
     buff= (char *) malloc(100*sizeof(char));
     for(int i = 0; i < 5; i++){
@@ -18,9 +20,13 @@ int main(){
         vector[i] = (char *) malloc((strlen(buff) + 1) * sizeof(char));
         strcpy(vector[i], buff);
     }
-
+    
 
     mostrarPersonas(vector, 5);
+    printf("Id a buscar: ");
+    scanf("%d", &id);
+    BuscarNombre(vector, id, 5);
+
 
     for(int i = 0; i < 5; i++){
         free(vector[i]);
@@ -36,4 +42,12 @@ void mostrarPersonas(char *vector[], int cantidad){
         printf("[%d]%s\n", i+1,vector[i]);
     }
      
+}
+
+void BuscarNombre(char *vector[], int id, int cantidad) {
+    if (id >= 0 && id < cantidad) {
+        printf("Nombre encontrado: %s\n", vector[id]);
+    } else {
+        printf("no se encontró el valor buscado\n");
+    }
 }
